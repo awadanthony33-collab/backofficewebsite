6bee388a21a87180230951f8109ee6fc355547c1
awadanthony33-collab <awadanthony33@gmail.com>
2026-03-19 15:12:18 +0200

    asd

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ConfigProvider } from '../node_modules/antd/es/index';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+           return( <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#ec0707', // 🔴 your red
+        },
+      }}
+    >
+ <LoginPage/>
+    </ConfigProvider>
+           )
+}
+
+export default App;
