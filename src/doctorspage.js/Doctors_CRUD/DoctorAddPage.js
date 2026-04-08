e3764c3cef7de5ca1026c4bef448cb98325e512b
awadanthony33-collab <awadanthony33@gmail.com>
2026-04-08 08:15:03 +0300

    hello

--- src/doctorspage.js/Doctors_CRUD/DoctorAddPage.js
+++ src/doctorspage.js/Doctors_CRUD/DoctorAddPage.js
@@ -12,7 +12,7 @@ const { TextArea } = Input;
 const DoctorAddPage = () => {
   const navigate       = useNavigate();
   const [form]         = Form.useForm();
-  const [saving, setSaving] = useState(false);
+  const [saving, setSaving] = useState(false);asdasd
 //save new doctors
   const handleSave = async (values) => {
     setSaving(true);
